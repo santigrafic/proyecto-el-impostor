081ab008ee123f671b72c7a10fe45b722f051d86
David Santiago Gavilan <santigrafic@gmail.com>
2026-05-22 19:04:22 +0200

    feat(project): implementada la creacion de pdfs

--- app/Services/PdfService.php
+++ app/Services/PdfService.php
@@ -2,13 +2,22 @@
 
 namespace App\Services;
 
+use Mpdf\Config\ConfigVariables;
+use Mpdf\Config\FontVariables;
 use Mpdf\Mpdf;
 
 class PdfService
 {
     protected Mpdf $mpdf;
+
     public function __construct()
     {
+        $defaultConfig = (new ConfigVariables())->getDefaults();
+        $fontDirs = $defaultConfig['fontDir'];
+
+        $defaultFontConfig = (new FontVariables())->getDefaults();
+        $fontData = $defaultFontConfig['fontdata'];
+
         $this->mpdf = new Mpdf([
             'mode' => 'utf-8',
             'format' => 'A4',
@@ -16,6 +25,18 @@ class PdfService
             'margin_bottom' => 16,
             'margin_left' => 15,
             'margin_right' => 15,
+
+            'fontDir' => array_merge($fontDirs, [
+                storage_path('fonts')
+            ]),
+
+            'fontdata' => $fontData + [
+                'pressstart2p' => [
+                    'R' => 'PressStart2P-Regular.ttf',
+                ]
+            ],
+
+            'default_font' => 'pressstart2p'
         ]);
     }
     public function render(string $view, array $data = []): string
--- routes/api.php
+++ routes/api.php
@@ -52,8 +52,7 @@ Route::apiResource('users', UserController::class);
 Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
 Route::middleware('auth:sanctum')->post('/update', [AuthController::class, 'update']);
 Route::middleware('auth:sanctum')->delete('/destroy', [UserController::class, 'destroy']);
-
-Route::get('me/pdf', [UserController::class, 'profilePdf']);
+Route::middleware('auth:sanctum')->get('me/pdf', [UserController::class, 'profilePdf']);
 
 Route::get('ranking', [UserController::class, 'ranking']);
 
